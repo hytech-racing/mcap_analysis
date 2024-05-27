1885a35aaef08b462b8f7f9d920b7baccf112a17
Ben Hall <rcmast3r1@gmail.com>
2024-05-27 03:09:01 -0400

    function to get start and end times in the log when a message is a specific value

--- mcap_processing.py
+++ mcap_processing.py
@@ -5,34 +5,127 @@ from pathlib import Path
 import io
 import os
 import glob
-
+from typing import Any
 from mcap_protobuf.decoder import DecoderFactory
 from mcap.reader import make_reader, McapReader
+import pandas as pd
+
+
+
+class MCAPMessage:
+    def __init__(self, schema, channel, message, proto_msg):
+        self.schema = schema
+        self.channel = channel
+        self.message = message
+        self.proto_msg = proto_msg
+        self.log_time = message.log_time
+
+def merge_dictionaries(*dicts):
+    merged_dict = {}
+    for dictionary in dicts:
+        for key, value in dictionary.items():
+            if key in merged_dict:
+                merged_dict[key].extend(value)
+            else:
+                merged_dict[key] = value.copy()
+    return merged_dict
+
+def convert_dict_to_pandas_df(all_msgs_dict: dict) -> pd.DataFrame:
+    records = []
+    for topic, messages in all_msgs_dict.items():
+        for msg in messages:
+            record = {
+                'topic': topic,
+                'schema': msg.schema,
+                'channel': msg.channel,
+                'message': msg.message,
+                'proto_msg': msg.proto_msg,
+                'log_time': pd.Timestamp(msg.log_time, unit='ns')
+            }
+            records.append(record)
+
+    # Create the DataFrame
+    df = pd.DataFrame(records)
+
+    return df
+
+def get_start_end_times(msg_df: pd.DataFrame, filter_topic: str, topic_attr_name: str, target_value: Any) -> list[tuple]:
+    """_summary_
+
+    Args:
+        msgs (dict): _description_
+        filter_topic (str): _description_
+        topic_attr_name (str): _description_
+
+    Returns:
+        list[tuple]: _description_
+    """
+    msgs = msg_df[msg_df['topic'] == filter_topic]
+
+    # Extract the member variable into a new column for easy comparison
+    msgs[topic_attr_name] = msgs['proto_msg'].apply(lambda x: getattr(x, topic_attr_name))
+    shifted_member_var = msgs[topic_attr_name].shift(1)
+    mask = msgs[topic_attr_name] != shifted_member_var
+    transitions = msgs[mask][['log_time', topic_attr_name]].to_numpy()
+
+    start_times = []
+    end_times = []
+    in_period = False
+    for transition in transitions:
+        if (transition[1] == target_value) and (not in_period):
+            in_period = True
+            start_times.append(transition[0])
+        elif (transition[1] is not target_value) and in_period:
+            in_period = False
+            end_times.append(transition[0])
+
+    if(len(start_times) > len(end_times)):
+        end_times.append(max(msgs[msg_df['log_time']]))
+
+    return list(zip(start_times, end_times))
+
 
 
 def get_min_value(time_series_msgs, attr_name):
+    """get the min value in a list or array of time series msgs
+
+    Args:
+        time_series_msgs (_type_): list / array type of time series msgs
+        attr_name (_type_): float / int attribute of the message that will be used to get the minimum value from
+
+    Returns:
+        _type_: min value of same attribute type 
+    """
     return min(getattr(msg, attr_name) for msg in time_series_msgs)
 
 
 # when braking, our front sus. pots should shorten
 # def analyze_sus_pots(sus_pot_timseries_msgs, ):
 
-def get_all_msgs_from_mcap_threaded(mcap_reader, num_threads):
-    stats = mcap_reader.get_summary().statistics
-    duration = (stats.message_end_time - stats.message_start_time)
-    
-    
+
+def get_all_msgs_from_mcap(mcap_reader) -> list[MCAPMessage]:
+    msgs = {}
+    for schema_, channel_, message_, proto_msg in mcap_reader.iter_decoded_messages():
+        if channel_.topic not in msgs:
+            msgs[channel_.topic] = []
+        msgs[channel_.topic].append(MCAPMessage(schema_, channel_, message_, proto_msg))
+    return msgs
+
+
 def get_topic_msgs_from_mcaps(mcap_readers, topic):
     msgs = []
     for reader in mcap_readers:
-        for schema_, channel_, message_, proto_msg in reader.iter_decoded_messages(topics = [topic]):
+        for schema_, channel_, message_, proto_msg in reader.iter_decoded_messages(
+            topics=[topic]
+        ):
             msgs.append(proto_msg)
     return msgs
+
 
 def open_files_in_folder(folder_path: str) -> list[McapReader]:
     # Create the full search pattern
     search_pattern = os.path.join(folder_path, "*.mcap")
-    
+
     # Get a list of all files matching the pattern
     files = glob.glob(search_pattern)
     # print(files)
@@ -46,7 +139,7 @@ def open_files_in_folder(folder_path: str) -> list[McapReader]:
     for file_path in files:
         try:
             print(os.path.join(os.getcwd(), file_path))
-            file = open(os.path.join(os.getcwd(), file_path), 'rb')
+            file = open(os.path.join(os.getcwd(), file_path), "rb")
             reader = make_reader(file, decoder_factories=[DecoderFactory()])
             mcap_readers.append(reader)
         except:
@@ -54,31 +147,35 @@ def open_files_in_folder(folder_path: str) -> list[McapReader]:
             pass
     return mcap_readers
 
+
 def main():
-    mcap_readers = open_files_in_folder(sys.argv[1])
-    topics = ["mcu_suspension_data", "sab_suspension_data"]
-    
+    mcap_readers = open_files_in_folder("/home/ben/mcap_analysis/mcaps/raw")
+    # topics = ["mcu_suspension_data", "sab_suspension_data"]
     # for topic in topics:
     # topic = "sab_suspension_data"
     # msgs = get_topic_msgs_from_mcaps(mcap_readers, topic)
     # print("min potentiometer_rl value for: ", topic, " is: ", get_min_value(msgs, "potentiometer_rl"))
     # msgs = get_topic_msgs_from_mcaps(mcap_readers, topic)
     # print("min potentiometer_rr value for: ", topic, " is: ", get_min_value(msgs, "potentiometer_rr"))
-    
+    msgs_total = {}
     for reader in mcap_readers:
-        get_all_msgs_from_mcap_threaded(reader, 3)
+        msgs_total = merge_dictionaries(msgs_total, get_all_msgs_from_mcap(reader))
+    df = convert_dict_to_pandas_df(msgs_total)
+
+    print(get_start_end_times(df, "mcu_status_data", "ecu_state", "5"))
     # topic = "mcu_suspension_data"
     # msgs = get_topic_msgs_from_mcaps(mcap_readers, topic)
     # print("min potentiometer_fl value for: ", topic, " is: ", get_min_value(msgs, "potentiometer_fl"))
     # msgs = get_topic_msgs_from_mcaps(mcap_readers, topic)
     # print("min potentiometer_fr value for: ", topic, " is: ", get_min_value(msgs, "potentiometer_fr"))
-    
+
     # print(mcap_readers)
     # for schema_, channel_, message_, proto_msg in mcap_readers[0].iter_decoded_messages():
     #     print(proto_msg)
-    # 
+    #
     # for topic_name in topics:
     #     get_topic_msgs_from_mcaps(mcaps, topic_name)
+
 
 if __name__ == "__main__":
     main()
